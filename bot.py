34e6fb28c3b54e513260d8bebab32d5138402dfb
fpband <82159763+fpband@users.noreply.github.com>
2022-03-22 17:03:31 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -90,6 +90,27 @@ async def SettingsBot(bot, cmd):
 		size_tag = "50%"
 	elif int(watermark_size) == 55:
 		size_tag = "55%"
+        elif int(watermark_size) == 60:
+		size_tag = "60%"
+        elif int(watermark_size) == 65:
+		size_tag = "65%"
+        elif int(watermark_size) == 70:
+		size_tag = "70%"
+        elif int(watermark_size) == 75:
+		size_tag = "75%"
+        elif int(watermark_size) == 80:
+		size_tag = "80%"
+        elif int(watermark_size) == 90:
+		size_tag = "90%"
+        elif int(watermark_size) == 95:
+		size_tag = "95%"
+        elif int(watermark_size) == 100:
+		size_tag = "100%"
+
+
+
+
+
 	else:
 		size_tag = "10%"
 	## --- Next --- ##
@@ -103,8 +124,11 @@ async def SettingsBot(bot, cmd):
 				[InlineKeyboardButton("Set Top Left", callback_data=f"position_5:5"), InlineKeyboardButton("Set Top Right", callback_data=f"position_main_w-overlay_w-5:5")],
 				[InlineKeyboardButton("Set Bottom Left", callback_data=f"position_5:main_h-overlay_h"), InlineKeyboardButton("Set Bottom Right", callback_data=f"position_main_w-overlay_w-5:main_h-overlay_h-5")],
 				[InlineKeyboardButton(f"Watermark Size - {size_tag}", callback_data="lel")],
-				[InlineKeyboardButton("10%", callback_data=f"size_10"), InlineKeyboardButton("15%", callback_data=f"size_15"), InlineKeyboardButton("20%", callback_data=f"size_20"), InlineKeyboardButton("25%", callback_data=f"size_25"), InlineKeyboardButton("30%", callback_data=f"size_30")],
-				[InlineKeyboardButton("35%", callback_data=f"size_35"), InlineKeyboardButton("40%", callback_data=f"size_40"), InlineKeyboardButton("45%", callback_data=f"size_45"), InlineKeyboardButton("50%", callback_data=f"size_50"), InlineKeyboardButton("55%", callback_data=f"size_55")]
+				[InlineKeyboardButton("10%", callback_data=f"size_10"), InlineKeyboardButton("15%", callback_data=f"size_15"), InlineKeyboardButton("20%", callback_data=f"size_20"), InlineKeyboardButton("25%", callback_data=f"size_25"), InlineKeyboardButton("30%", callback_data=f"size_30"), InlineKeyboardButton("35%", callback_data=f"size_35")],
+				[InlineKeyboardButton("40%", callback_data=f"size_40"), InlineKeyboardButton("45%", callback_data=f"size_45"), InlineKeyboardButton("50%", callback_data=f"size_50"), InlineKeyboardButton("55%", callback_data=f"size_55"), InlineKeyboardButton("60%", callback_data=f"size_60"), .InlineKeyboardButton("65%", callback_data=f"size_65")],
+		                [InlineKeyboardButton("70%", callback_data=f"size_70"), InlineKeyboardButton("75%", callback_data=f"size_75"), InlineKeyboardButton("80%", callback_data=f"size_80"), InlineKeyboardButton("85%", callback_data=f"size_85"), InlineKeyboardButton("90%", callback_data=f"size_90"), .InlineKeyboardButton("95%", callback_data=f"size_95")],
+                                [InlineKeyboardButton("100%", callback_data=f"size_100")],
+			
 			]
 		)
 	)
@@ -490,6 +514,25 @@ async def button(bot, cmd: CallbackQuery):
 			size_tag = "50%"
 		elif int(watermark_size) == 55:
 			size_tag = "55%"
+                elif int(watermark_size) == 60:
+		        size_tag = "60%"
+                elif int(watermark_size) == 65:
+		        size_tag = "65%"
+                elif int(watermark_size) == 70:
+		        size_tag = "70%"
+                elif int(watermark_size) == 75:
+		        size_tag = "75%"
+                elif int(watermark_size) == 80:
+		        size_tag = "80%"
+                elif int(watermark_size) == 85:
+		        size_tag = "85%"
+                elif int(watermark_size) == 90:
+		        size_tag = "90%"
+                elif int(watermark_size) == 95:
+		        size_tag = "95%"
+                elif int(watermark_size) == 100:
+		        size_tag = "100%"
+
 		else:
 			size_tag = "10%"
 		try:
@@ -498,14 +541,17 @@ async def button(bot, cmd: CallbackQuery):
 				disable_web_page_preview=True,
 				parse_mode="Markdown",
 				reply_markup=InlineKeyboardMarkup(
-					[
-						[InlineKeyboardButton(f"Watermark Position - {position_tag}", callback_data="lol")],
-						[InlineKeyboardButton("Set Top Left", callback_data=f"position_5:5"), InlineKeyboardButton("Set Top Right", callback_data=f"position_main_w-overlay_w-5:5")],
-						[InlineKeyboardButton("Set Bottom Left", callback_data=f"position_5:main_h-overlay_h"), InlineKeyboardButton("Set Bottom Right", callback_data=f"position_main_w-overlay_w-5:main_h-overlay_h-5")],
-						[InlineKeyboardButton(f"Watermark Size - {size_tag}", callback_data="lel")],
-						[InlineKeyboardButton("10%", callback_data=f"size_10"), InlineKeyboardButton("15%", callback_data=f"size_15"), InlineKeyboardButton("20%", callback_data=f"size_20"), InlineKeyboardButton("25%", callback_data=f"size_25"), InlineKeyboardButton("30%", callback_data=f"size_30")],
-						[InlineKeyboardButton("35%", callback_data=f"size_35"), InlineKeyboardButton("40%", callback_data=f"size_40"), InlineKeyboardButton("45%", callback_data=f"size_45"), InlineKeyboardButton("50%", callback_data=f"size_50"), InlineKeyboardButton("55%", callback_data=f"size_55")]
-					]
+								[
+				[InlineKeyboardButton(f"Watermark Position - {position_tag}", callback_data="lol")],
+				[InlineKeyboardButton("Set Top Left", callback_data=f"position_5:5"), InlineKeyboardButton("Set Top Right", callback_data=f"position_main_w-overlay_w-5:5")],
+				[InlineKeyboardButton("Set Bottom Left", callback_data=f"position_5:main_h-overlay_h"), InlineKeyboardButton("Set Bottom Right", callback_data=f"position_main_w-overlay_w-5:main_h-overlay_h-5")],
+				[InlineKeyboardButton(f"Watermark Size - {size_tag}", callback_data="lel")],
+				[InlineKeyboardButton("10%", callback_data=f"size_10"), InlineKeyboardButton("15%", callback_data=f"size_15"), InlineKeyboardButton("20%", callback_data=f"size_20"), InlineKeyboardButton("25%", callback_data=f"size_25"), InlineKeyboardButton("30%", callback_data=f"size_30"), InlineKeyboardButton("35%", callback_data=f"size_35")],
+				[InlineKeyboardButton("40%", callback_data=f"size_40"), InlineKeyboardButton("45%", callback_data=f"size_45"), InlineKeyboardButton("50%", callback_data=f"size_50"), InlineKeyboardButton("55%", callback_data=f"size_55"), InlineKeyboardButton("60%", callback_data=f"size_60"), .InlineKeyboardButton("65%", callback_data=f"size_65")],
+		                [InlineKeyboardButton("70%", callback_data=f"size_70"), InlineKeyboardButton("75%", callback_data=f"size_75"), InlineKeyboardButton("80%", callback_data=f"size_80"), InlineKeyboardButton("85%", callback_data=f"size_85"), InlineKeyboardButton("90%", callback_data=f"size_90"), .InlineKeyboardButton("95%", callback_data=f"size_95")],
+                                [InlineKeyboardButton("100%", callback_data=f"size_100")],
+			
+			                                        ]
 				)
 			)
 		except MessageNotModified:
