e395141a121962f1ec87e093764aaeb38e0b8017
fpband <82159763+fpband@users.noreply.github.com>
2022-04-22 14:46:45 +0430

    Update force_sub_handler.py

--- core/handlers/force_sub_handler.py
+++ core/handlers/force_sub_handler.py
@@ -17,7 +17,7 @@ async def handle_force_subscribe(bot, cmd):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+                text="Sorry Sir, You are Banned to use me. Contact my [Support Admin](https://t.me/FarshidBand).",
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
@@ -25,14 +25,15 @@ async def handle_force_subscribe(bot, cmd):
     except UserNotParticipant:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="**Please Join My Updates Channel to use this Bot!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!",
+            text="**• برای استفاده از ربات باید در کانال زیر عضو 
+شوید سپس "عضو شدم" را کلیک کنید.👇**",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
+                        InlineKeyboardButton("🌺 عضویت 🌺", url=invite_link.invite_link)
                     ],
                     [
-                        InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refreshmeh")
+                        InlineKeyboardButton("✅ عضو شدم 👍", callback_data="refreshmeh")
                     ]
                 ]
             ),
@@ -42,7 +43,7 @@ async def handle_force_subscribe(bot, cmd):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+            text="Something went Wrong. Contact my [Support Admin](https://t.me/FarshidBand).",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
