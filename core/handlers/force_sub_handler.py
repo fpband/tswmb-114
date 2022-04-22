63199ad12271ac6a26b71d85a4dc0866360b6e73
fpband <82159763+fpband@users.noreply.github.com>
2022-04-22 12:10:55 +0430

    Update force_sub_handler.py

--- core/handlers/force_sub_handler.py
+++ core/handlers/force_sub_handler.py
@@ -17,7 +17,7 @@ async def handle_force_subscribe(bot, cmd):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+                text="• دسترسی شما به ربات مسدود شد. [گرارش](https://t.me/FarshidBand).",
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
@@ -29,10 +29,10 @@ async def handle_force_subscribe(bot, cmd):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
+                        InlineKeyboardButton("🌺 عضویت 🌺", url=invite_link.invite_link)
                     ],
                     [
-                        InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refreshmeh")
+                        InlineKeyboardButton("✅ عضو شدم 👍", callback_data="refreshmeh")
                     ]
                 ]
             ),
@@ -42,7 +42,7 @@ async def handle_force_subscribe(bot, cmd):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+            text="⁦**⚠️⁩ مشکلی پیش آمد. [گزارش](https://t.me/FarshidBand).**",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
